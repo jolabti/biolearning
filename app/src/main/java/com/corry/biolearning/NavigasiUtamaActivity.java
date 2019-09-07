fc57623eb81d7d9fc1b2f0ca58e127ae128cd988
johan <johan@bareksa.com>
2019-09-07 11:15:11 +0700

    layouting + Intent halaman navigasi utama

---
+++ app/src/main/java/com/corry/biolearning/NavigasiUtamaActivity.java
@@ -0,0 +1,66 @@
+package com.corry.biolearning;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class NavigasiUtamaActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    AppCompatButton btnSains, btnQuiz, btnForum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_nav);
+
+        btnSains = findViewById(R.id.button_materi_sains_id);
+        btnQuiz = findViewById(R.id.button_materi_quiz_id);
+        btnForum = findViewById(R.id.button_materi_forum_id);
+
+        btnSains.setOnClickListener(this);
+        btnQuiz.setOnClickListener(this);
+        btnForum.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.button_materi_sains_id:
+
+
+                Toast.makeText(this, "SAINS BUTTON", Toast.LENGTH_SHORT).show();
+                Intent intentSains = new Intent(this,MateriActivity.class);
+                pindahPage(intentSains);
+
+
+                break;
+            case R.id.button_materi_quiz_id:
+                Toast.makeText(this, "QUIZ BUTTON", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button_materi_forum_id:
+                Toast.makeText(this, "FORUM BUTTON", Toast.LENGTH_SHORT).show();
+
+                break;
+
+
+        }
+
+    }
+
+
+    public void pindahPage(Intent intent){
+        startActivity(intent);
+
+    }
+}
