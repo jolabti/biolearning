f6f90d99c85285a997618e95344eeac05ddd022b
johan <johan@bareksa.com>
2019-09-12 09:34:06 +0700

    login splash and firebase auth

---
+++ app/src/main/java/com/corry/biolearning/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.corry.biolearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private  Handler myHandler = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_splash);
+
+        myHandler = new Handler();
+
+        myHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent gotoLogin = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(gotoLogin);
+
+            }
+        }, 3000);
+    }
+}
